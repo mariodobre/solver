1180de595d010d18e3e6f7847c0803b5f4a9db1d
Mario Dobre <mariodobre1@gmail.com>
2020-10-26 06:06:39 +0200

    hello

---
+++ blog/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
+
+# Create your views here.
+#def home(request):
+#	return render(request, 'home.html', {})
+
+
+
+class HomeView(ListView):
+	model = Post
+	template_name = 'home.html'
+
+
+class ArticleDetail(DetailView):
+	model = Post
+	template_name = 'details.html'
